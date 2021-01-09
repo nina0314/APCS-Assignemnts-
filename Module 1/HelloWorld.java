a6d64aede88b304936cc894dd5dff8f1188a7eb7
nina0314 <77189499+nina0314@users.noreply.github.com>
2021-01-09 17:45:52 +0900

    Create HelloWorld.java

---
+++ Module 1/HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * Write a description of class HelloWorld here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class HelloWorld
+{
+    public static void main (String[] args)
+    {
+        System.out.print("Hello, Virtual World!");
+        System.out.println();
+        System.out.print("This is so cool!");
+        System.out.println();
+        System.out.println("It is a great day for programming.");
+    }//end of main method
+}//end of class
