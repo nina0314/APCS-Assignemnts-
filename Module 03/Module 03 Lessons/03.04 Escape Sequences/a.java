545f39119969f435026875c24311a4c96f9bbee5
nina0314 <77189499+nina0314@users.noreply.github.com>
2021-01-09 18:15:45 +0900

    Add files via upload

---
+++ Module 03/Module 03 Lessons/03.04 Escape Sequences/a.java
@@ -0,0 +1,40 @@
+
+/**
+ * Write a description of class a here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class a
+{
+    public static void main(String[ ] args)
+    {
+        //local variables
+        String row01 = "       /@@@@@@@@@@@@@\\ ";
+        String row02 = "\n      /@      @   \\@@@\\";
+        String row03 = "\n     /@@      @    \\@@@\\"; 
+        String row04 = "\n    /@@@      @     \\@@@\\";
+        String row05 = "\n @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@";
+        String row06 = "\n @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@";
+        String row07 = "\n @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@";
+        String row08 = "\n @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@";
+        String row09 = "\n      @@@@@@@@     @@@@@@@@";
+        String row10 = "\n       @@@@@@       @@@@@@";
+        String row11 = "\n                             Nina Nielsen";
+        
+        System.out.print(row01);
+        System.out.print(row02);
+        System.out.print(row03); 
+        System.out.print(row04);
+        System.out.print(row05);
+        System.out.print(row06);
+        System.out.print(row07);
+        System.out.print(row08);
+        System.out.print(row09);
+        System.out.print(row10);
+        System.out.print(row11);
+    
+
+    
+    }
+}
