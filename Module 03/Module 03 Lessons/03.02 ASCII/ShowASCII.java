6850e48c8ab70665a1a12cd7e8f2ac789f8c56ea
nina0314 <77189499+nina0314@users.noreply.github.com>
2021-01-09 18:13:47 +0900

    Create ShowASCII.java

---
+++ Module 03/Module 03 Lessons/03.02 ASCII/ShowASCII.java
@@ -0,0 +1,77 @@
+
+/**
+ * Write a description of class ShowASCII here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class ShowASCII
+{
+ public static void main(String[] args)
+ {
+   //cast a character literal to its integer ASCII value
+   String a ="Just\nsay\n\"NO\"!"; 
+   int n = a.length();
+   System.out.println("Value of n is " + n);
+   
+   System.out.println("The ASCII code for " + 'A' + " is " + (int)'A');
+   System.out.println("The ASCII code for " + '9' + " is " + (int)'9');
+   System.out.println();
+   //cast an integer ASCII value to its corresponding character literal
+   System.out.println("93 is the ASCII code for " + (char)93);
+   System.out.println("173 is the ASCII code for " + (char)173);
+   System.out.println();
+   // concatenate characters to form a message;
+   System.out.println("My message: " + (char)72 + (char)105 + (char)33);
+   
+   
+   char letterA = 'A';
+   char aDash = '-';
+   char blankSpace = ' '; //there is a blank space between the quotes
+   char eighthDigit = '8';
+   System.out.println(letterA);
+   System.out.println(aDash);
+   System.out.println(blankSpace);
+   System.out.println(eighthDigit);
+   
+   System.out.println("83 is the ASCII code for " + (char)83);
+   System.out.println("101 is the ASCII code for " + (char)101);
+   System.out.println("110 is the ASCII code for " + (char)110);
+   System.out.println("100 is the ASCII code for " + (char)100);
+   System.out.println("32 is the ASCII code for " + (char)32);
+   System.out.println("121 is the ASCII code for " + (char)121);
+   System.out.println("111 is the ASCII code for " + (char)111);
+   System.out.println("117 is the ASCII code for " + (char)117);
+   System.out.println("114 is the ASCII code for " + (char)114);
+   System.out.println("32 is the ASCII code for " + (char)32);
+   System.out.println("116 is the ASCII code for " + (char)116);
+   System.out.println("101 is the ASCII code for " + (char)101);
+   System.out.println("97 is the ASCII code for " + (char)97);
+   System.out.println("99 is the ASCII code for " + (char)99);
+   System.out.println("104 is the ASCII code for " + (char)104);
+   System.out.println("101 is the ASCII code for " + (char)101);
+   System.out.println("114 is the ASCII code for " + (char)114);
+   System.out.println("32 is the ASCII code for " + (char)32);
+   System.out.println("97 is the ASCII code for " + (char)97);
+   System.out.println("110 is the ASCII code for " + (char)110);
+   System.out.println("32 is the ASCII code for " + (char)32);
+   System.out.println("101 is the ASCII code for " + (char)101);
+   System.out.println("109 is the ASCII code for " + (char)109);
+   System.out.println("97 is the ASCII code for " + (char)97);
+   System.out.println("105 is the ASCII code for " + (char)105);
+   System.out.println("108 is the ASCII code for " + (char)108);
+   System.out.println("32 is the ASCII code for " + (char)32);
+   System.out.println("116 is the ASCII code for " + (char)116);
+   System.out.println("111 is the ASCII code for " + (char)111);
+   System.out.println("100 is the ASCII code for " + (char)100);
+   System.out.println("97 is the ASCII code for " + (char)97);
+   System.out.println("121 is the ASCII code for " + (char)121);
+   System.out.println("33 is the ASCII code for " + (char)33);
+   
+   System.out.println("You can’t always \"print\" what you want."); 
+   System.out.println("But if you try sometimes,"); 
+   System.out.println("You can \"print\" what you need!");
+
+
+ }//end of main method
+}//end of class
