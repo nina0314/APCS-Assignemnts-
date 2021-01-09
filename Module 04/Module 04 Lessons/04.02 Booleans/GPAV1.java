d3f5eaa967a6b1893a740837869a88fd3099d1d6
nina0314 <77189499+nina0314@users.noreply.github.com>
2021-01-09 18:26:25 +0900

    Create GPAV1.java

---
+++ Module 04/Module 04 Lessons/04.02 Booleans/GPAV1.java
@@ -0,0 +1,33 @@
+
+/**
+ * This program illustrates the use of a boolean expression
+ * to determine whether a player is eligible for competition.
+ *
+ * @author Nina Nielsen
+ * @version 15/07/18
+ */
+import java.util.Scanner;
+public class GPAV1
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("GPA Calculator");
+        System.out.println();
+        System.out.println("Enter 5 grades separated by a space (e.g. 4 3 2 1 4)): ");
+        int grade1 = in.nextInt();
+        int grade2 = in.nextInt();
+        int grade3 = in.nextInt();
+        int grade4 = in.nextInt();
+        int grade5 = in.nextInt();
+
+        double average = (grade1 + grade2 + grade3 + grade4 + grade5)/5.0;
+        boolean isEligible = average >= 2.0;               //this is a boolean expression
+                                                           //which evaluates to true or false
+
+        System.out.println("GPA: " + average);
+        System.out.println("Eligible: " + isEligible);
+        
+}
+}
