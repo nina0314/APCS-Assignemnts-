b1c6d81e4bb969041c79f31547f0a241368e3db4
nina0314 <77189499+nina0314@users.noreply.github.com>
2021-01-09 17:59:32 +0900

    Create CalculationsV5.java

---
+++ Module 2/Lessons/02.04 Arithmetic Expressions/CalculationsV5.java
@@ -0,0 +1,97 @@
+
+/**
+ * The Calculations class performs addition, subtraction,
+ * multiplication, division, and modulus operations on integers and
+ * decimals.
+ *  
+ * @author Nina Nielsen
+ * @version 16/06/18       
+ */
+public class CalculationsV5
+{
+    public static void main(String[ ] args)
+    {
+        // Declare integer variables
+        
+        int anum = 14;
+        int bnum = 190;
+        int cnum = 45;
+        int dnum = 3;
+        int fnum = 72;
+        int iNum1 = 25;
+        int iNum2 = 9;
+        
+        //Declare double variables
+        double dNum1=43.21;
+        double dNum2 =3.14;
+        
+        //Addition
+        System.out.println("Addition");
+        System.out.print(anum + "+" + bnum + "=");  
+        System.out.println(anum+bnum);
+        System.out.println(cnum + "+" + dnum + "=" + (cnum+dnum));
+        //System.out.println();
+        System.out.println();
+        // Subtraction
+        System.out.println("Subtraction");
+        System.out.print( anum + "-" + bnum + "=");
+        System.out.println(anum-bnum);
+        System.out.println( cnum + "-" + dnum + "=" + (cnum-dnum));
+        //System.out.println();
+        System.out.println();
+        // Multiplication
+        System.out.println("Multiplication");
+        System.out.println(anum + "*" + fnum + "=" ); 
+        System.out.println(anum*fnum);
+        System.out.println(cnum + "*" + dnum + "=");
+        System.out.println(cnum*dnum);
+        //System.out.println();
+        System.out.println();
+        // Division
+        System.out.println("Division");
+        System.out.println(cnum + "+" + dnum +"=");
+        System.out.println(cnum/dnum);
+        System.out.println(fnum + "/" + anum + "=" );
+        System.out.println(fnum/anum);
+        //System.out.println();
+         System.out.println();
+        // Modulus operator
+        System.out.println("Modulus");
+        System.out.println( fnum + "%" + dnum + "=" );
+        System.out.println(fnum%dnum);
+        System.out.println( bnum + "%" + anum + "=");
+        System.out.println (bnum%anum);
+        //System.out.println();
+        System.out.println();
+        //Multiplication
+        System.out.println("Multiplication!");
+        System.out.println (dnum + "*" + fnum + "=");
+        System.out.println(dnum*fnum);
+        //Division
+        System.out.println();
+        System.out.println("Division");
+        System.out.println(fnum + "/" + dnum + "=");
+        System.out.print(fnum/dnum);
+        System.out.println();
+        System.out.println();
+        //Subtraction 
+        System.out.println();
+        System.out.println("Subtraction");
+        System.out.println(anum + "-" + dnum + "=");
+        System.out.println(anum-dnum);
+        // Addition 
+        System.out.println("Addition");
+        System.out.println(iNum1 + " + " + iNum2 + " = " + (iNum1 + iNum2));
+        System.out.println(dNum1 + " + " + dNum2 + " = " + (dNum1 + dNum2));
+        System.out.println();
+        
+      
+        
+        // 2.03 Additional int Equations
+        
+        
+        //2.04 Additional double Equations
+        
+               
+    } // end of main method
+} // end of class
