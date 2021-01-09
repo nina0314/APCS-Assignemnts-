34db094ae2fc6cc86f757714761c5126a40696a0
nina0314 <77189499+nina0314@users.noreply.github.com>
2021-01-09 17:58:35 +0900

    Create CalculationsV3.java

---
+++ Module 2/Lessons/02.03 Arithmetic Expressions/CalculationsV3.java
@@ -0,0 +1,55 @@
+/**
+ * The Calculations class performs addition, subtraction,
+ * multiplication, division, and modulus operations on integers and
+ * decimals.
+ *  
+ * @author Nina Nielsen
+ * @version 16/06/18       
+ */
+public class CalculationsV3
+{
+    public static void main(String[ ] args)
+    {
+        // Declare integer variables
+        
+        int a = 14;
+        int b = 190;
+        //Addition
+        System.out.println("Addition");
+        System.out.print(a + "+" + b + "=");  
+        System.out.println(a+b);
+        System.out.println(43.21 + "+" + 3.14 + "=" + (43.21+3.14));
+        //System.out.println();
+        System.out.println();
+        // Subtraction
+        System.out.println("Subtraction");
+        System.out.println( "11 - 9 - 25=" + (11-9-25) );
+        System.out.println( "3.14 - 10.0=" + (3.14-10.0) );
+        //System.out.println();
+        System.out.println();
+        // Multiplication
+        System.out.println("Multiplication");
+        System.out.println( "25 * 9=" + (25*9) ); 
+        System.out.println("3.14 * 10.0 * 10.0=" + (3.14*10.0*10.0) );
+        //System.out.println();
+        System.out.println();
+        // Division
+        System.out.println("Division");
+        System.out.println( "9 / 25=" + (9/25) );
+        System.out.println( "43.21 / 10.0=" +(43.21/10/0) );
+        //System.out.println();
+         System.out.println();
+        // Modulus operator
+        System.out.println("Modulus");
+        System.out.println( "11 % 9=" + (11%9));
+        System.out.println( "10.0 % 3.14=" + (10.0%3.14));
+        //System.out.println();
+        System.out.println();
+        // 2.03 Additional int Equations
+        
+        
+        //2.04 Additional double Equations
+        
+               
+    } // end of main method
+} // end of class
