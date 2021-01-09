1c8c7bc425830b81f866daae24b374543662f9cd
nina0314 <77189499+nina0314@users.noreply.github.com>
2021-01-09 18:00:56 +0900

    Create GradesV1.java

---
+++ Module 2/Lessons/02.05 Arithmetic Expressions/GradesV1.java
@@ -0,0 +1,46 @@
+/**
+* The purpose of this program is to
+* calculate the average of four grades
+*
+* @author Meg Abyte
+* @version 06/11/17
+*/
+public class GradesV1
+{    
+    public static void main(String[ ] args)
+    {    
+        int test1 = 96;        //test score 1
+        System.out.println("Test Score 1: 96%");
+        int test2 = 78;        //test score 2
+        System.out.println("Test Score 2: 78%");
+        int test3 = 85;        //test score 3
+        System.out.println("Test Score 3: 85%");
+        int test4 = 87;        //test score 4
+        System.out.println("Test Score 4: 87%");
+        double average;  //average of four test scores
+        
+                    
+        //calculate the average grade and print the results
+        average = (test1 + test2 + test3 + test4) / 4.0;
+        System.out.println("Average Score: " + average);
+        
+        double x = 2.5;
+        double y = (2 * x);
+        double z;
+        
+        z = (int) (x + (2 * y));
+        System.out.println("Answer of this is:" + z);
+        
+        int f =10;
+        int b =14;
+        int c = 25;
+        int d;
+        
+        
+
+        
+        
+        
+        
+    }//end of main method
+}//end of class
