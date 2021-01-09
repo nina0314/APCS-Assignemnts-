a1f74aeb7feb2a917a9eef192c83359dd5f1e5b0
nina0314 <77189499+nina0314@users.noreply.github.com>
2021-01-09 18:19:36 +0900

    Create ArtWork.java

---
+++ Module 03/Module 03 Assignments/03.04 ASCII Art/ArtWork.java
@@ -0,0 +1,41 @@
+/**
+ * Creating ASCII art using keyboard characters. 
+ *
+ * @Nina Nielsen
+ * @24/06/18
+ */
+public class ArtWork
+{
+    public static void main(String[ ] args)
+    {
+        //local variables
+        String row01 = "        /@@@@@@@@@@@@@\\ ";
+        String row02 = "\n       /@      @   \\@@@\\";
+        String row03 = "\n      /@@      @    \\@@@\\"; 
+        String row04 = "\n     /@@@      @     \\@@@\\";
+        String row05 = "\n @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@";
+        String row06 = "\n @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@";
+        String row07 = "\n @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@";
+        String row08 = "\n @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@";
+        String row09 = "\n      @@@@@@@@     @@@@@@@@";
+        String row10 = "\n       @@@@@@       @@@@@@";
+        String row11 = "\n                             Nina Nielsen";
+        
+        System.out.print(row01);
+        System.out.print(row02);
+        System.out.print(row03); 
+        System.out.print(row04);
+        System.out.print(row05);
+        System.out.print(row06);
+        System.out.print(row07);
+        System.out.print(row08);
+        System.out.print(row09);
+        System.out.print(row10);
+        System.out.print(row11);
+        
+        System.out.println();
+        System.out.println('8' + '2');
+        System.out.println("128" + "32");
+    
+    }//end of main method
+}//end of class 
